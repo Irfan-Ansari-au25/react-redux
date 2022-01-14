414dde4351b5dd0d974d32d50df51af5f7808170
Irfan-Ansari-au25 <irfan398649@gmail.com>
2022-01-15 00:18:38 +0530

    Implemented react-redux store

---
+++ src/components/CakeContainer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { buyCake } from "../redux";
+import { connect } from "react-redux";
+
+const CakeContainer = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <h2>No of Cakes {props.noOfCakes}</h2>
+      <button onClick={props.buyCake}> Buy Cake</button>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    noOfCakes: state.noOfCakes,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    buyCake: () => dispatch(buyCake()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CakeContainer);
